f6b20a6ba7a2048cb8f3b7ed81709fad133b2b69
Romina Natalia Martinez <martinezromina@outlook.com.ar>
2023-10-24 10:43:32 -0300

    se genera funcionalidad

--- asset/js/app_index.js
+++ asset/js/app_index.js
@@ -34,3 +34,55 @@ function HorasExtras() {
 
 }
 
+
+function Vacaciones() {
+
+    let SalarioBruto = parseFloat(document.getElementById("SalarioNeto").value);
+
+    let CDiasDeVacaciones = parseFloat(document.getElementById("CantidadDiasDeVacaciones").value);
+
+
+
+    var Monto, Vacaciones;
+
+    Monto = SalarioBruto / 25;
+
+    Vacaciones = Monto * CDiasDeVacaciones;
+
+
+    let resultado_vacaciones = document.getElementById("resultado_vacaciones");
+    resultado_vacaciones.value = Vacaciones.toFixed(2);
+    
+}
+
+function Feriado() {
+
+   let ValorDiaDeTrabajo = parseFloat(document.getElementById("valorDiaTrabajo").value);
+
+   ValorDiaFeriado = ValorDiaDeTrabajo * 2;
+
+   let resultado_feriado = document.getElementById("resultado_feriado");
+
+   resultado_feriado.value = ValorDiaFeriado.toFixed(2);
+    
+    
+}
+
+function Sac(){
+
+    let MejorSalarioBruto = parseFloat(document.getElementById("MejorSalarioNeto").value);
+
+    let MesesTrabajados = parseFloat(document.getElementById("MesesTrabajados").value);
+
+    
+
+    Aguinaldo = MejorSalarioBruto / 12 * MesesTrabajados;
+
+
+
+    let resultado_sac = document.getElementById("resultado_sac");
+
+    resultado_sac.value = Aguinaldo.toFixed(2);
+
+
+}
