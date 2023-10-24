913d674d4682f35fc3ffb96032e8d4263d3cf45b
Romina Natalia Martinez <martinezromina@outlook.com.ar>
2023-10-24 10:20:41 -0300

    funcionalidad horasExtras.html

--- asset/js/app_index.js
+++ asset/js/app_index.js
@@ -1,6 +1,6 @@
 
 
-function horasExtras() {
+function HorasExtras() {
 
     let SalarioBruto = parseFloat(document.getElementById("SalarioNeto").value);
 
@@ -10,17 +10,17 @@ function horasExtras() {
 
 
 
-    let HorasPorMes,HorasPorSemana, ValorHora, HoraExtra50, HoraExtra100;
+    var HorasPorMes,HorasPorSemana, ValorHora, HoraExtra50, HoraExtra100;
 
-    HorasPorSemana == HorasPorDia*CDiasPorSemana;
+    HorasPorSemana = HorasPorDia*CDiasPorSemana;
 
-    HorasPorMes == HorasPorSemana * 4;
+    HorasPorMes = HorasPorSemana * 4;
 
-    ValorHora == SalarioBruto/HorasPorMes;
+    ValorHora = SalarioBruto/HorasPorMes;
 
-    HoraExtra50 == ValorHora * 1.50;
+    HoraExtra50 = ValorHora * 1.50;
 
-    HoraExtra100 == ValorHora * 2;
+    HoraExtra100 = ValorHora * 2;
 
 
     let resultado_hora = document.getElementById("resultado_hora");
